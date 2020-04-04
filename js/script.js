c04a8d39f860618e4ee7eaabc7e2eb36aaa25058
Icez <pierrekeller75@gmail.com>
2020-04-04 18:20:26 +0200

    Fix Reset + Dice (random)

--- js/script.js
+++ js/script.js
@@ -72,32 +72,95 @@ function resetAll() {
   document.querySelector('.totalRed').innerHTML = restNb
   document.querySelector('.totalOrange').innerHTML = restNb
   document.querySelector('.totalRedDk').innerHTML = restNb
+  document.getElementById('totalSac').innerHTML = restNb
 }
+
+// function lancerTotal() {
+//   let random = (document.querySelector('.inputNumber').value)
+//   let totalRandom = ( random * parseInt(document.getElementById('0of99').innerHTML))
+//   let total4 = ( 4 * parseInt(document.getElementById('0of4').innerHTML))
+//   let total6 = ( 6 * parseInt(document.getElementById('0of6').innerHTML))
+//   let total8 = ( 8 * parseInt(document.getElementById('0of8').innerHTML))
+//   let total10 = ( 10 * parseInt(document.getElementById('0of10').innerHTML))
+//   let total12 = ( 12 * parseInt(document.getElementById('0of12').innerHTML))
+//   let total20 = ( 20 * parseInt(document.getElementById('0of20').innerHTML))
+//   let totalRandomAll = 1 + Math.floor(Math.random() * totalRandom)
+//   let total4Random =  1 + Math.floor(Math.random() * total4)
+//   let total6Random = 1 + Math.floor(Math.random() * total6)
+//   let total8Random = 1 + Math.floor(Math.random() * total8)
+//   let total10Random = 1 + Math.floor(Math.random() * total10)
+//   let total12Random = 1 + Math.floor(Math.random() * total12)
+//   let total20Random = 1 + Math.floor(Math.random() * total20)
+//   document.getElementById('changeRandomOfBlack').innerHTML = random
+//   document.querySelector('.totalGreen').innerHTML = total4Random
+//   document.querySelector('.totalBlue').innerHTML = total6Random
+//   document.querySelector('.totalPurple').innerHTML = total8Random
+//   document.querySelector('.totalBlack').innerHTML = totalRandomAll
+//   document.querySelector('.totalRed').innerHTML = total10Random
+//   document.querySelector('.totalOrange').innerHTML = total12Random
+//   document.querySelector('.totalRedDk').innerHTML = total20Random
+//   let totalAffichage = parseInt(total4Random + total6Random + total8Random + total10Random + total12Random + total20Random + totalRandomAll)
+//   document.getElementById('totalSac').innerHTML = totalAffichage
+// }
 
 function lancerTotal() {
   let random = (document.querySelector('.inputNumber').value)
-  let totalRandom = parseInt(document.getElementById('0of99').innerHTML)
-  let total4 = parseInt(document.getElementById('0of4').innerHTML)
-  let total6 = parseInt(document.getElementById('0of6').innerHTML)
-  let total8 = parseInt(document.getElementById('0of8').innerHTML)
-  let total10 = parseInt(document.getElementById('0of10').innerHTML)
-  let total12 = parseInt(document.getElementById('0of12').innerHTML)
-  let total20 = parseInt(document.getElementById('0of20').innerHTML)
-  let totalRandomAll = totalRandom * Math.floor(Math.random() * random + 1)
-  let total4Random = total4 * Math.floor(Math.random() * 4 + 1)
-  let total6Random = total6 * Math.floor(Math.random() * 6 + 1)
-  let total8Random = total8 * Math.floor(Math.random() * 8 + 1)
-  let total10Random = total10 * Math.floor(Math.random() * 10 + 1)
-  let total12Random = total12 * Math.floor(Math.random() * 12 + 1)
-  let total20Random = total20 * Math.floor(Math.random() * 20 + 1)
-  document.getElementById('changeRandomOfBlack').innerHTML = random
-  document.querySelector('.totalGreen').innerHTML = total4Random
-  document.querySelector('.totalBlue').innerHTML = total6Random
-  document.querySelector('.totalPurple').innerHTML = total8Random
-  document.querySelector('.totalBlack').innerHTML = totalRandomAll
-  document.querySelector('.totalRed').innerHTML = total10Random
-  document.querySelector('.totalOrange').innerHTML = total12Random
-  document.querySelector('.totalRedDk').innerHTML = total20Random
-  let totalAffichage = parseInt(total4Random + total6Random + total8Random + total10Random + total12Random + total20Random + totalRandomAll)
+  let totalRandom = ( random * parseInt(document.getElementById('0of99').innerHTML))
+  let total4 = ( 4 * parseInt(document.getElementById('0of4').innerHTML))
+  let total6 = ( 6 * parseInt(document.getElementById('0of6').innerHTML))
+  let total8 = ( 8 * parseInt(document.getElementById('0of8').innerHTML))
+  let total10 = ( 10 * parseInt(document.getElementById('0of10').innerHTML))
+  let total12 = ( 12 * parseInt(document.getElementById('0of12').innerHTML))
+  let total20 = ( 20 * parseInt(document.getElementById('0of20').innerHTML))
+  let totalRandomAll = 1 + Math.floor(Math.random() * totalRandom)
+  let total4Random =  1 + Math.floor(Math.random() * total4)
+  let total6Random = 1 + Math.floor(Math.random() * total6)
+  let total8Random = 1 + Math.floor(Math.random() * total8)
+  let total10Random = 1 + Math.floor(Math.random() * total10)
+  let total12Random = 1 + Math.floor(Math.random() * total12)
+  let total20Random = 1 + Math.floor(Math.random() * total20)
+  if (total4 == 0) {
+    document.querySelector('.totalGreen').innerHTML = 0
+  } else {
+    document.querySelector('.totalGreen').innerHTML = total4Random
+  }
+  if (total6 == 0) {
+    document.querySelector('.totalBlue').innerHTML = 0
+  } else {
+    document.querySelector('.totalBlue').innerHTML = total6Random
+  }
+  if (total8 == 0) {
+    document.querySelector('.totalPurple').innerHTML = 0
+  } else {
+    document.querySelector('.totalPurple').innerHTML = total8Random
+  }
+  if (total10 == 0) {
+    document.querySelector('.totalRed').innerHTML = 0
+  } else {
+    document.querySelector('.totalRed').innerHTML = total10Random
+  }
+  if (total12 == 0) {
+    document.querySelector('.totalOrange').innerHTML = 0
+  } else {
+    document.querySelector('.totalOrange').innerHTML = total12Random
+  }
+  if (total20 == 0) {
+    document.querySelector('.totalRedDk').innerHTML = 0
+  } else {
+    document.querySelector('.totalRedDk').innerHTML = total20Random
+  }
+  if (totalRandom == 0) {
+    document.querySelector('.totalBlack').innerHTML = 0
+  } else {
+    document.querySelector('.totalBlack').innerHTML = totalRandomAll
+  }
+  let total4Affichage = parseInt(document.querySelector('.totalGreen').innerHTML)
+  let total6Affichage = parseInt(document.querySelector('.totalBlue').innerHTML)
+  let total8Affichage = parseInt(document.querySelector('.totalPurple').innerHTML)
+  let total10Affichage = parseInt(document.querySelector('.totalRed').innerHTML)
+  let total12Affichage = parseInt(document.querySelector('.totalOrange').innerHTML)
+  let total20Affichage = parseInt(document.querySelector('.totalRedDk').innerHTML)
+  let totalRandomAffichage = parseInt(document.querySelector('.totalBlack').innerHTML)
+  let totalAffichage = parseInt(total4Affichage + total6Affichage + total8Affichage + total10Affichage + total12Affichage + total20Affichage + totalRandomAffichage)
   document.getElementById('totalSac').innerHTML = totalAffichage
 }
