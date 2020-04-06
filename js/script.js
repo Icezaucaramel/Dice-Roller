04d8667fc0d714766b05edaa1c903b281187ea86
Icez <pierrekeller75@gmail.com>
2020-04-06 16:37:33 +0200

    Minor update

--- js/script.js
+++ js/script.js
@@ -18,35 +18,21 @@ function boutonPlus(de) {
   document.getElementById("0of" + de).innerHTML = count
 }
 
-// function lancerTotal() {
-//   let random = (document.querySelector('.inputNumber').value)
-//   let totalRandom = (random * parseInt(document.getElementById('0of99').innerHTML))
-//   let total4 = (4 * parseInt(document.getElementById('0of4').innerHTML))
-//   let total6 = (6 * parseInt(document.getElementById('0of6').innerHTML))
-//   let total8 = (8 * parseInt(document.getElementById('0of8').innerHTML))
-//   let total10 = (10 * parseInt(document.getElementById('0of10').innerHTML))
-//   let total12 = (12 * parseInt(document.getElementById('0of12').innerHTML))
-//   let total20 = (20 * parseInt(document.getElementById('0of20').innerHTML))
-//   let total = parseInt(total4 + total6 + total8 + total10 + total12 + total20 + totalRandom)
-//   totalAffichage = 1 + Math.floor(Math.random() * total)
-//   document.getElementById('totalSac').innerHTML = totalAffichage
-// }
-
 let interval = setInterval(intervalFunction, 1500)
 function intervalFunction() {
   let randomNumbersHolders = 1 + Math.floor(Math.random() * 99)
   document.getElementsByName('changePlaceHolder')[0].placeholder = parseInt(randomNumbersHolders)
 }
 
-textBox = document.querySelector('.inputNumber')
-textBox.onkeypress = function(e) {
-  e = e || window.event
-  let charCode = (typeof e.which == "undefined") ? e.keyCode : e.which
-  let charStr = String.fromCharCode(charCode)
-  if (/\D/.test(charStr)) {
-      return false
-  }
-}
+// textBox = document.querySelector('.inputNumber')
+// textBox.onkeypress = function(e) {
+//   e = e || window.event
+//   const charCode = (typeof e.which == "undefined") ? e.keyCode : e.which
+//   const charStr = String.fromCharCode(charCode)
+//   if (/\D/.test(charStr)) {
+//       return false
+//   }
+// }
 
 function checkNumberFieldLength(){
   let lenghtOfInput = document.querySelector('.inputNumber').value
@@ -74,34 +60,6 @@ function resetAll() {
   document.querySelector('.totalRedDk').innerHTML = restNb
   document.getElementById('totalSac').innerHTML = restNb
 }
-
-// function lancerTotal() {
-//   let random = (document.querySelector('.inputNumber').value)
-//   let totalRandom = ( random * parseInt(document.getElementById('0of99').innerHTML))
-//   let total4 = ( 4 * parseInt(document.getElementById('0of4').innerHTML))
-//   let total6 = ( 6 * parseInt(document.getElementById('0of6').innerHTML))
-//   let total8 = ( 8 * parseInt(document.getElementById('0of8').innerHTML))
-//   let total10 = ( 10 * parseInt(document.getElementById('0of10').innerHTML))
-//   let total12 = ( 12 * parseInt(document.getElementById('0of12').innerHTML))
-//   let total20 = ( 20 * parseInt(document.getElementById('0of20').innerHTML))
-//   let totalRandomAll = 1 + Math.floor(Math.random() * totalRandom)
-//   let total4Random =  1 + Math.floor(Math.random() * total4)
-//   let total6Random = 1 + Math.floor(Math.random() * total6)
-//   let total8Random = 1 + Math.floor(Math.random() * total8)
-//   let total10Random = 1 + Math.floor(Math.random() * total10)
-//   let total12Random = 1 + Math.floor(Math.random() * total12)
-//   let total20Random = 1 + Math.floor(Math.random() * total20)
-//   document.getElementById('changeRandomOfBlack').innerHTML = random
-//   document.querySelector('.totalGreen').innerHTML = total4Random
-//   document.querySelector('.totalBlue').innerHTML = total6Random
-//   document.querySelector('.totalPurple').innerHTML = total8Random
-//   document.querySelector('.totalBlack').innerHTML = totalRandomAll
-//   document.querySelector('.totalRed').innerHTML = total10Random
-//   document.querySelector('.totalOrange').innerHTML = total12Random
-//   document.querySelector('.totalRedDk').innerHTML = total20Random
-//   let totalAffichage = parseInt(total4Random + total6Random + total8Random + total10Random + total12Random + total20Random + totalRandomAll)
-//   document.getElementById('totalSac').innerHTML = totalAffichage
-// }
 
 function lancerTotal() {
   let random = (document.querySelector('.inputNumber').value)
@@ -164,3 +122,25 @@ function lancerTotal() {
   let totalAffichage = parseInt(total4Affichage + total6Affichage + total8Affichage + total10Affichage + total12Affichage + total20Affichage + totalRandomAffichage)
   document.getElementById('totalSac').innerHTML = totalAffichage
 }
+
+
+let textBox = document.querySelector('.inputNumber');
+    textBox.addEventListener('keypress', function(e) {
+        let charcode = e.which;
+        // ASCII from 0 to 9 = 48 to 57
+        if (!(charcode >= 48 && charcode <= 57)) {
+            // Cancel the event
+            if( e.preventDefault) e.preventDefault();
+            return false;
+        }
+    });
+    
+// textBox = document.querySelector('.inputNumber')
+// textBox.onkeypress = function(e) {
+//   e = e || window.event
+//   const charCode = (typeof e.which == "undefined") ? e.keyCode : e.which
+//   const charStr = String.fromCharCode(charCode)
+//   if (/\D/.test(charStr)) {
+//       return false
+//   }
+// }
